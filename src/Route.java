eb963af35926180a710a64f5cfc1145901d56717
Bendeguz <bendeguzvarju@gmail.com>
2021-10-23 13:08:25 +0200

    refactor

--- src/Route.java
+++ src/Route.java
@@ -1,25 +1,13 @@
-import java.util.ArrayList;
-
 public class Route {
-		public int start;
-		public int finish;
-		public long distance;
-		public boolean toBeFound;
-		
-		public Route(int s, int f) {
-			start = s;
-			finish = f;
-			distance = 0;
-			toBeFound = false;
-		}
-		
-		void distanceCounter(ArrayList<Node> nodes) {
-			for(Node n1 : nodes) {
-				for(Node n2 : nodes) {
-					if(n1.index == start && n2.index == finish) {
-						distance = n1.getDistanceBetween(n2);
-					}
-				}
-			}
-		}
+    public int index1;
+    public int index2;
+
+
+    public boolean contains(int index) {
+        if(index == index1 || index == index2) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
